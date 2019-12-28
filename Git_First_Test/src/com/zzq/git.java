9e249c6059db5c5c2cee68f486414843e0d47dde
赵忠泉 <zzqphm@163.com>
2019-12-28 20:17:20 +0800

    test

---
+++ Git_First_Test/src/com/zzq/git.java
@@ -0,0 +1,13 @@
+package com.zzq;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!");
+    }
+}
